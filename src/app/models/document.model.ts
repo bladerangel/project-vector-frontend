7129189defd5aa82cb460e2e2eded749470ef00d
bladerangel <bladerangel10@gmail.com>
2021-04-29 14:36:34 -0300

    Create document model

---
+++ src/app/models/document.model.ts
@@ -0,0 +1,7 @@
+export class Document {
+  constructor(public id: number,
+              public identificationCode: string,
+              public description: string,
+              public personRequested: string) {
+  }
+}
