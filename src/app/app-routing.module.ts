67c8dbda58acd538aaa6720131ed7401f8e639a5
bladerangel <bladerangel10@gmail.com>
2021-04-29 14:36:21 -0300

    Create routes lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/login/login.module').then(module => module.LoginModule),
+
+  },
+  {
+    path: 'document',
+    loadChildren: () => import('./modules/document/document.module').then(module => module.DocumentModule),
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
